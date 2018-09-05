fca1095cb7aa5a8dd8693ad9a6992f5dd6fdd2ee
nicolas <nicolastpi10@gmail.com>
2018-09-05 20:23:40 -0300

    verificando test en verde con CI

--- src/test/java/model/EjemploTest.java
+++ src/test/java/model/EjemploTest.java
@@ -1,7 +1,6 @@
 package model;
 
 import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Test;
 
@@ -14,7 +13,8 @@ public class EjemploTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		assertEquals("Uno es igual a uno", 1, 1);
+		//fail("Not yet implemented");
 	}
 
 }
