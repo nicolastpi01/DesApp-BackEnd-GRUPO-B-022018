2a5b443c4decf73bee29f7e0ca2fd875af1c4128
nicolas <nicolastpi10@gmail.com>
2018-09-05 20:15:06 -0300

    verificando test con CI

---
+++ src/test/java/model/EjemploTest.java
@@ -0,0 +1,20 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EjemploTest {
+	
+	@Before
+	public void setUp() {
+		
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
