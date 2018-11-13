e068b6847dd8ff6273ed27e23456610e4643aaf4
guido <guidopujadas@gmail.com>
2018-11-13 14:43:54 -0300

    test arch

--- src/test/java/architectural/ArqTest.java
+++ src/test/java/architectural/ArqTest.java
@@ -1,14 +1,25 @@
 package architectural;
 
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
 import org.junit.Test;
 import org.springframework.web.bind.annotation.RestController;
-import com.tngtech.archunit.core.domain.JavaClasses;
-import com.tngtech.archunit.core.importer.ClassFileImporter;
-import com.tngtech.archunit.junit.AnalyzeClasses;
-import com.tngtech.archunit.lang.ArchRule;
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 
-@AnalyzeClasses(packages = "")
+import com.tngtech.archunit.core.domain.JavaClass;
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.domain.JavaMethod;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.lang.ArchRule;
+
+import service.AuctionController;
+
+import org.springframework.stereotype.Service;
+
 public class ArqTest {
 	
 	@Test
@@ -19,4 +30,15 @@ public class ArqTest {
 
 		    myRule.check(classes);
 	}
+	
+	
+	@Test
+	public void All_services_have_Service_Annotation() {
+		JavaClasses classes = new ClassFileImporter().importPackages("service");
+		ArchRule myRule = classes().that().haveSimpleNameEndingWith("Service").
+				should().beAnnotatedWith(Service.class);
+
+		    myRule.check(classes);
+	}
+	
 }
